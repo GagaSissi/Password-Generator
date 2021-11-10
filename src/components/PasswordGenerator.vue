c806525dbcaf45e9b9c398411892f1bb8c1adcbe
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-11-10 13:07:20 +0100

    lunchtime update

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -7,15 +7,25 @@
   </section>
   <section>
     <form v-on:submit.prevent="add">
-      <input type="checkbox" class="buttonCheckbox" id="labelID1" />
+      <input type="checkbox" class="buttonLowercase" id="labelID1" />
       <label class="labelButton" for="labelID1">Lowercase</label>
+      <input type="checkbox" class="buttonUppercase" id="labelID2" />
+      <label class="labelButton" for="labelID2">Uppercase</label>
+      <input type="checkbox" class="buttonNumbers" id="labelID3" />
+      <label class="labelButton" for="labelID3">Numbers</label>
+      <input type="checkbox" class="buttonSymbols" id="labelID4" />
+      <label class="labelButton" for="labelID4">Symbols</label>
     </form>
     <form v-on:submit.prevent="add">
       <p>Password Lenght:</p>
-      <input type="range" class="slidingTool" id="labelID2" />
-      <label class="labelSlider" for="labelID2" min="8" max="35"
-        >irgendein wert</label
-      >
+      <input
+        type="range"
+        class="slidingTool"
+        id="labelrangeID1"
+        min="8"
+        max="35"
+      />
+      <label class="labelSlider" for="labelrangeID1">irgendein wert</label>
     </form>
   </section>
 </template>
@@ -42,28 +52,53 @@ export default {
   --accent-color: #2292a4;
   --success-color: #cbe896;
 }
-.buttonCheckbox {
+
+section {
+  font-size: 1.25em;
+}
+
+p {
+  margin: 3em;
+}
+
+input {
   all: unset;
+  /* margin: 1em; */
+  padding: 0.1em 0.4em;
 }
 
 .labelButton {
-  padding: 0 0.2rem;
+  padding: 0 1rem;
   border: 0.2rem solid var(--accent-color);
   border-radius: 0.2rem;
 }
 
-.buttonCheckbox:checked + .labelButton {
-  background-color: var(--dark-color);
+.buttonLowercase:checked + .labelButton {
+  background-color: var(--accent-color);
   color: var(--light-color);
 }
 
-.buttonCheckbox:focus + .labelButton {
+.buttonUppercase:checked + .labelButton {
+  background-color: var(--accent-color);
+  color: var(--light-color);
+}
+
+.buttonNumbers:checked + .labelButton {
+  background-color: var(--accent-color);
+  color: var(--light-color);
+}
+
+.buttonSymbols:checked + .labelButton {
+  background-color: var(--accent-color);
+  color: var(--light-color);
+}
+
+.buttonLowercase:focus + .labelButton {
   border: 0.2rem solid var(--accent-color);
 }
 
-p {
-  margin-top: 7rem;
-}
+/* Sliding Bar*/
+/* Länge einstellen, wie? */
 
 .slidingTool {
   -webkit-appearance: none;
@@ -76,8 +111,8 @@ p {
 .slidingTool::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
-  width: 1rem;
-  height: 1rem;
+  width: 1.5rem;
+  height: 1.5rem;
   background: var(--accent-color);
   border-radius: 50%;
 }
